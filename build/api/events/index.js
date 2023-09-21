b89ec31ee8baa3e19cc3aa967fe56f7a16eab965
owujib <owujibfavour@gmail.com>
2023-09-22 00:42:22 +0100

    removed git cache

--- build/api/events/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_events_1 = require("node:events");
-class Event extends node_events_1.EventEmitter {
-}
-exports.default = Event;
--- build/models/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// // import { Knex } from 'knex';
-Object.defineProperty(exports, "__esModule", { value: true });
-const database_1 = require("../config/database");
-exports.default = database_1.databaseConfiguration;
